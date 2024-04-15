33179db4b241fb6d5259070161df2ab4ad3d4b31
Yash <yashpandey1106@gmail.com>
2024-04-15 17:18:33 +0530

    add user signup

---
+++ app/controllers/profiles_controller.rb
@@ -0,0 +1,18 @@
+class ProfilesController < ApplicationController
+  def new
+  end
+
+  def create
+    @profile = current_user.build_profile(profile_params)
+    if @profile.save
+      redirect_to root_path
+    else
+    end
+  end
+
+  private
+
+  def profile_params
+    params.require(:profile).permit(:full_name, :bio, :dob, :phone_number, :location)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  get 'static/index'
+  root 'static#index'
+  get 'signup', to: "registrations#new"
+  post 'signup', to: "registrations#create"
+  get 'new_profile', to: "profiles#new"
+  post 'profile', to: "profiles#create"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
