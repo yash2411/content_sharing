3ff30d7f73a79f36d8121745d0b076f036a20780
Yash <yashpandey1106@gmail.com>
2024-04-19 14:03:50 +0530

    add email confirmation mailer

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,6 +1,15 @@
 class RegistrationsController < ApplicationController
-    skip_before_action :authenticate_user
+    skip_before_action :authenticate_user, except: :verify_account
+
+    skip_before_action :check_account_status
     include Authenticated
+
+    def verify_account
+        if current_user.activated?
+            redirect_to root_path
+        else
+        end
+    end
 
     def new
         @user = User.new
@@ -9,9 +18,11 @@ class RegistrationsController < ApplicationController
     def create
         @user = User.new(user_params)
         if @user.save
+            EmailConfirmationMailer.with(email: @user.email, host: request.base_url).send_email_activation_link.deliver_later
             session[:user_id] = @user.id
-            redirect_to new_profile_path
+            redirect_to verify_account_path
         else
+
         end
     end
 
@@ -21,9 +32,24 @@ class RegistrationsController < ApplicationController
         render json: { unique: user.nil? }
     end
 
+    def verify_email
+        user = User.find_by_id(params[:id])
+        if user.update(activated: true)
+            flash[:success] = 'Your email has been verified'
+            redirect_to root_path
+        else 
+            flash[:danger] = 'Account not found'
+            redirect_to root_path
+        end
+    end
+
     private
 
     def user_params
         params.require(:user).permit(:email, :password, :password_confirmation, :terms_of_service)
+    end
+
+    def check_account_status
+        redirect_to verify_account_path if User.find_by_id(session[:user_id])
     end
 end
---
+++ spec/mailers/previews/email_confirmation_mailer_preview.rb
@@ -0,0 +1,4 @@
+# Preview all emails at http://localhost:3000/rails/mailers/email_confirmation
+class EmailConfirmationMailerPreview < ActionMailer::Preview
+
+end
