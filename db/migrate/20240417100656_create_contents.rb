2a7662684cad5cca18f2bb9c934203437e7c79c3
Yash <yashpandey1106@gmail.com>
2024-04-24 17:22:43 +0530

    content model

--- db/migrate/20240417100656_create_contents.rb
+++ db/migrate/20240417100656_create_contents.rb
@@ -1,6 +1,7 @@
 class CreateContents < ActiveRecord::Migration[7.0]
   def change
     create_table :contents do |t|
+      t.string :type
       t.string :title
       t.references :user, null: false, foreign_key: true
 
